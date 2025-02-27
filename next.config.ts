bec3717bb8ec2f36f0e89abe3f14db147793dcbf
freedisch <freeproduc@gmail.com>
2025-02-27 14:26:37 +0200

    bug:fix rendering bugs
    Signed-off-by: freedisch <freeproduc@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+      GOOGLE_API_KEY: process.env.GOOGLE_API_KEY, // pulls from .env file
+    },
+  images: {
+      domains: ['maps.googleapis.com'],
+  },
 };
 
 export default nextConfig;
